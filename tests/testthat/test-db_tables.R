4082b532ceba2d260926017c0081c9efb5b9e856
Shawn Taylor <sdtaylor@users.noreply.github.com>
2020-09-29 16:36:48 -0600

    Add test db for travis (#7)
    * add test database
    
    - phenomet db in sqlite with the majority of years and plants removed
    - quick test on functional group function
    - turn on travis

--- tests/testthat/test-db_tables.R
+++ tests/testthat/test-db_tables.R
@@ -1,4 +1,5 @@
 
+set_phenometR_backend('testthat-db')
 
 test_that('Dates must be sequental', {
   expect_error(parse_dates(years=NULL, start_date='2015-01-01', end_date='2014-01-01'))
@@ -10,4 +11,11 @@ test_that('Dates must be YYYY-MM-DD',{
   expect_error(parse_dates(years=NULL, start_date=NULL, end_date='2012-1-01'))
   expect_silent(parse_dates(years=NULL, start_date='2010-01-01', end_date='2014-01-01'))
   expect_silent(parse_dates(years=NULL, start_date=NULL, end_date=NULL))
+})
+
+test_that('Phenophase functional group returns',{
+  expect_silent(get_fg_phenophase('PG'))
+  expect_silent(get_fg_phenophase('DS'))
+  expect_silent(get_fg_phenophase('ES'))
+  expect_silent(get_fg_phenophase('SU'))
 })
