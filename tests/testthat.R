75dc8f82e4fb5ddc5d1381468ff51d5f6d35558c
Shawn <shawntaylor@weecology.org>
2020-09-29 14:27:28 -0600

    make a new db options for testthat
    makes the test.db file path point from the tests/testthat dir

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,6 +1,4 @@
 library(testthat)
 library(phenometR)
 
-set_phenometR_backend('test-db')
-
 test_check("phenometR")
--- tests/testthat/test-db_tables.R
+++ tests/testthat/test-db_tables.R
@@ -1,4 +1,5 @@
 
+set_phenometR_backend('testthat-db')
 
 test_that('Dates must be sequental', {
   expect_error(parse_dates(years=NULL, start_date='2015-01-01', end_date='2014-01-01'))
