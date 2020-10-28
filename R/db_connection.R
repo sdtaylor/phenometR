20018bcbbf8746152768d511457601e36910111e
Shawn Taylor <sdtaylor@users.noreply.github.com>
2020-10-28 10:22:18 -0600

    move test db to new location (#11)
    * move test db to new location
    
    the inst/extdata folder documented at https://r-pkgs.org/data.html
    and accessed via the system.file command.
    
    remove fixed path options for test db

--- R/db_connection.R
+++ R/db_connection.R
@@ -23,11 +23,11 @@ is_error <- function(x){
 #' Use this to set to a local sqlite db
 #' for testing only
 #'
-#' @param backend string. oen of 'jornada-server' or 'test-db'
+#' @param backend string. one of 'jornada-server' or 'test-db'
 #'
 #' @export
 set_phenometR_backend = function(backend){
-  if(!backend %in% c('jornada-server','test-db','testthat-db')){
+  if(!backend %in% c('jornada-server','test-db')){
     stop('Invalid backend: ',backend)
   }
   options(phenometR.backend = backend)
@@ -90,14 +90,11 @@ db_connect <- function(){
   # Use a local sqlite db for testing purposes
   backend <- getOption('phenometR.backend')
   if(backend == 'test-db'){
-    con = try(DBI::dbConnect(RSQLite::SQLite(), dbname = getOption('phenometR.test_db_file')))
+    db_file = system.file("extdata", "test.db", package = "phenometR")
+    con = try(DBI::dbConnect(RSQLite::SQLite(), dbname = db_file))
     if(is_error(con)) stop('Error load test.db database')
     return(con)
-  } else if(backend == 'testthat-db'){
-    con = try(DBI::dbConnect(RSQLite::SQLite(), dbname = getOption('phenometR.testthat_db_file')))
-    if(is_error(con)) stop('Error load test.db database')
-    return(con)
-  }
+  } 
   
   # Otherwise connect to the primary mysql db
   connection_attempts = 3
